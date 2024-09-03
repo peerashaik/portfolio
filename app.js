07028821ff64745f6405fcf1640e14c348134403
Shaik Peera <49202095+peerashaik@users.noreply.github.com>
2024-09-03 11:18:25 +0530

    Add files via upload
    updates

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-    // $(window).scroll(function() {
-    //     var scroll = $(window).scrollTop();
-    //     if(scroll >= 100) {
-    //         $('.navbar').addClass('sticky-top');
-    //     } else {
-    //         $('.navbar').removeClass('sticky-top');
-    //     }
-    // });
-
     $('.navbar-toggler').click(function(){
         $(this).toggleClass('open');
         $('.sticky-top').toggleClass('mobileNav');
