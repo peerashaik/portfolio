0a0f05cdca3991a9de815469437024ca4413744d
Shaik Peera <49202095+peerashaik@users.noreply.github.com>
2024-09-07 20:11:28 +0530

    Add files via upload
    t

--- app.js
+++ app.js
@@ -10,15 +10,4 @@ $(document).ready(function() {
     let date = new Date();
     let year = date.getFullYear();
     document.getElementById('date').innerHTML = year;
-
-
-    function loadDoc() {
-        alert(123);
-        const xhttp = new XMLHttpRequest();
-        xhttp.onload = function() {
-            document.getElementById("ajax-load").innerHTML = this.responseText;
-        }
-        xhttp.open("GET", "https://peerashaik.github.io/ui/shaiks-ajax.txt");
-        xhttp.send();
-    }
-})
+});
