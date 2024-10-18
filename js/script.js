231759f77ea543b6c818144802a0a912cba074d5
Shaik <peershaik786@gmail.com>
2024-10-18 15:39:45 +0530

    Latest changes

--- js/script.js
+++ js/script.js
@@ -18,6 +18,11 @@ $(document).ready(function() {
         }
     });
 
+var url = window.location.href;
+url = url.split('.html')[0];
+//url = url.substring(0, url.lastIndexOf("."));
+window.history.replaceState( null, null, url );
+
     document.addEventListener('contextmenu', event => event.preventDefault());
 
     window.addEventListener('scroll', e => {
