985b7f78aa30a0432c195f5992b926d1fb9e85fc
Shaik <peershaik786@gmail.com>
2024-10-29 22:29:45 +0530

    Latest changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+window.addEventListener('load', function() {
     'use strict';
 
     //url re-wrtite
@@ -12,31 +12,10 @@ $(document).ready(function() {
     let year = date.getFullYear();
     document.getElementById('date').innerHTML = year;
 
-    //mobile menu nav
-    $('.navbar-toggler').click(function() {
-        $(this).toggleClass('open');
-        $('.navbar').toggleClass('mobileNav');
-
-        if($(this).hasClass('open')) {
-            $('.menu').addClass('');
-        } else {
-            $('.menu').removeClass('slideout');
-        }
-    });
-
     //right click disbaled
     document.addEventListener('contextmenu', event => event.preventDefault());
 
     window.addEventListener('scroll', e => {
-        //scrolling lines
-        // var scrolLine = $(window).scrollTop();
-
-        // if (scrolLine >= 250 && scrolLine <= 450) {
-        //     $('#lines > div').addClass('full');
-        // } else {
-        //     $('#lines > div').removeClass('full');
-        // }
-
         //smooth back to top
         const btnScrollToTop = document.querySelector(".back-to-top");
 
@@ -53,17 +32,31 @@ $(document).ready(function() {
 
 });
 
+$(document).ready(function() {
+    //mobile menu nav
+    $('.navbar-toggler').click(function() {
+        $(this).toggleClass('open');
+        $('.navbar').toggleClass('mobileNav');
+
+        if($(this).hasClass('open')) {
+            $('.menu').addClass('');
+        } else {
+            $('.menu').removeClass('slideout');
+        }
+    });
+});
+
 //site visitor counter
-var counterContainer = document.querySelector(".website-counter");
-var visitCount = localStorage.getItem("page_view");
+// var counterContainer = document.querySelector(".website-counter");
+// var visitCount = localStorage.getItem("page_view");
 
-// Check if page_view entry is present
-if (visitCount) {
-    visitCount = Number(visitCount) + 1;
-    localStorage.setItem("page_view", visitCount);
-} else {
-    visitCount = 1;
-    localStorage.setItem("page_view", 1);
-}
+// // Check if page_view entry is present
+// if (visitCount) {
+//     visitCount = Number(visitCount) + 1;
+//     localStorage.setItem("page_view", visitCount);
+// } else {
+//     visitCount = 1;
+//     localStorage.setItem("page_view", 1);
+// }
 
-counterContainer.innerHTML = visitCount;
+// counterContainer.innerHTML = visitCount;
