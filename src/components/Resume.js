b8b3b3919e23c915668672e98e080d8778d7cff4
Shaik <peershaik786@gmail.com>
2025-05-23 18:19:15 +0530

    Updated new resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Resume = () => {
     const onButtonClick = () => {
-        const pdfUrl = "Certified-Magento2-Frontend-Developer-PeeraShaik.pdf";
+        const pdfUrl = "Senior-User-Interface-Designer and Developer-PeeraShaik.pdf";
         const link = document.createElement("a");
         link.href = pdfUrl;
-        link.download = "Certified-Magento2-Frontend-Developer-PeeraShaik.pdf";
+        link.download = "Senior-User-Interface-Designer and Developer-PeeraShaik.pdf";
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
