a6b26b4293ea784276f94308167fdeece77ed962
Shaik <peershaik786@gmail.com>
2025-07-23 05:53:35 +0530

    latest changes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Resume = () => {
     const onButtonClick = () => {
-        const pdfUrl = "Certified Magento2 Theme Designer and Developer.pdf";
+        const pdfUrl = "User Interface Designer - PeeraShaik.pdf";
         const link = document.createElement("a");
         link.href = pdfUrl;
-        link.download = "Certified Magento2 Theme Designer and Developer.pdf";
+        link.download = "User Interface Designer - PeeraShaik.pdf";
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
